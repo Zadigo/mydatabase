2d5549ea19aaca932ba2072011fbec1e3cd0cb1d
Zadigo <pendenquejohn@gmail.com>
2024-01-28 23:06:59 +0100

    "sheets" to "datasources"

--- my_database/urls.py
+++ my_database/urls.py
@@ -5,7 +5,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('api/v1/slides/', include('slides.api.urls')),
-    path('api/v1/sheets/', include('sheets.api.urls')),
+    path('api/v1/datasources/', include('datasources.api.urls')),
     path('connections/api/', include('connections.api.urls')),
     path('admin/', admin.site.urls),
 ]
