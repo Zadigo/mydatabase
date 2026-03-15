7930a5ed2cc9bc72e9b9bcb36307c60d86a55a3f
Zadigo <pendenquejohn@gmail.com>
2026-03-15 20:47:09 +0100

    Move to oxlint

--- djangobackend/dbschemas/api/serializers.py
+++ djangobackend/dbschemas/api/serializers.py
@@ -10,7 +10,10 @@ from rest_framework import serializers
 
 class DatabaseSchemaSerializer(serializers.ModelSerializer):
     tables = DatabaseTableSerializer(
-        many=True, read_only=True, source='databasetable_set')
+        many=True, 
+        read_only=True, 
+        source='databasetable_set'
+    )
 
     class Meta:
         model = DatabaseSchema
